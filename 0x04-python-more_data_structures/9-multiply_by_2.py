1e4d9ecf197ed9177673106f368ac483198c2633
o2g1 <o2g.firm@gmail.com>
2022-06-08 16:58:16 +0000

    high level

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
-def multiply_by_2(a_dictionary):
-    copyd = a_dictionary.copy()
-    for i, num in copyd.items():
-        copyd[i] = num * 2
-    return copyd
+def multiply_by_2(my_dict):
+    tmp_dict = my_dict.copy()
+    for x in tmp_dict.keys():
+        tmp_dict[x] *= 2
+    return (tmp_dict)
