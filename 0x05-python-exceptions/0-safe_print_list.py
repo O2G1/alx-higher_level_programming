0ad01b271f5169989afa1cfb621e3a7a917251e4
o2g1 <o2g.firm@gmail.com>
2022-06-21 00:56:12 +0000

    high level

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    p = 0
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            p += 1
-        except:
-            pass
-    print()
-    return p
+    try:
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
+        pass
