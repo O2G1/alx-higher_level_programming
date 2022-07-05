8c6f2d288a6f5ffd19f27d2d3f7a57b0ef286851
o2g1 <o2g.firm@gmail.com>
2022-07-05 16:15:39 +0000

    high level

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """
-    2-read_lines.py
-    Reads n lines of a text file (UTF8) and prints it to stdout.
+    2-append_write: append_write()
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads n lines of a text file (UTF8) and prints it to stdout."""
-    n_lines = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            n_lines += 1
-
-    with open(filename, encoding='utf-8') as f:
-        if nb_lines <= 0 or nb_lines >= n_lines:
-            print(f.read(), end="")
-
-        n_lines = 0
-        for line in f:
-            n_lines += 1
-            if n_lines <= nb_lines:
-                print(line, end="")
-                continue
-            break
+def append_write(filename="", text=""):
+    """
+        append_write appends a string at the end of a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be appended to the file.
+        Returns: number of characters written.
+    """
+    with open(filename, "a", encoding='utf=8') as a_file:
+        return a_file.write(text)
