3d2f913023a9d3f227af30ccf710c871977c16b4
o2g1 <geesofttechnology@gmail.com>
2022-09-06 01:16:35 +0000

    shell

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-/*
-  print JS is amazing
-*/
 const myVar = 'Javascript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-/*
-  print 3 lines with loop
-*/
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const line of args) {
-  console.log(line);
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const el of arr) {
+  console.log(el);
 }
